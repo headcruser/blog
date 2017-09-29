5e9c0c0a9deea03fd6efb7a2e3cb46f4e2875877
headcruser <headcruser@gmail.com>
2017-09-29 12:54:49 -0500

    Añadiendo link de Documentacion en redes Sociales

--- compiler/08d79af9e97cbcbad163b5e7f7da2590d3088bfc_0.file.footer.inc.tpl.php
+++ compiler/08d79af9e97cbcbad163b5e7f7da2590d3088bfc_0.file.footer.inc.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32-dev-11, created on 2017-09-26 15:59:39
+/* Smarty version 3.1.32-dev-11, created on 2017-09-28 16:42:18
   from "/var/www/html/blog/styles/templates/overall/footer.inc.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32-dev-11',
-  'unifunc' => 'content_59cabfbbb5ae12_80830074',
+  'unifunc' => 'content_59cd6cba8bee63_07867688',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '08d79af9e97cbcbad163b5e7f7da2590d3088bfc' => 
     array (
       0 => '/var/www/html/blog/styles/templates/overall/footer.inc.tpl',
-      1 => 1506459430,
+      1 => 1506634921,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,14 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_59cabfbbb5ae12_80830074 (Smarty_Internal_Template $_smarty_tpl) {
+function content_59cd6cba8bee63_07867688 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <section class="redes-sociales">
 	<div class="contenedor">
 		<a href="#" class="twitter"> <i class="fa fa-twitter"></i></a>
 		<a href="#" class="facebook"> <i class="fa fa-facebook"></i></a>
 		<a href="#" class="youtube"> <i class="fa fa-youtube-play"></i></a>
-		<a href="#" class="git"> <i class="fa fa-github"></i></a>
+		<a href="https://headcruser.github.io/blog/" class="git"> <i class="fa fa-github"></i></a>
 		<a href="#" class="ins"> <i class="fa fa-instagram"></i></a>
 	</div>
 	<div class="derechos">Copyright © 2017 company name. All Rights Reserved.</div>		
--- styles/templates/overall/footer.inc.tpl
+++ styles/templates/overall/footer.inc.tpl
@@ -3,7 +3,7 @@
 		<a href="#" class="twitter"> <i class="fa fa-twitter"></i></a>
 		<a href="#" class="facebook"> <i class="fa fa-facebook"></i></a>
 		<a href="#" class="youtube"> <i class="fa fa-youtube-play"></i></a>
-		<a href="#" class="git"> <i class="fa fa-github"></i></a>
+		<a href="https://headcruser.github.io/blog/" class="git"> <i class="fa fa-github"></i></a>
 		<a href="#" class="ins"> <i class="fa fa-instagram"></i></a>
 	</div>
 	<div class="derechos">Copyright © 2017 company name. All Rights Reserved.</div>		
