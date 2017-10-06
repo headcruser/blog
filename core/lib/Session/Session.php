ec4e08b4a1cb49a6d27f6fde9a4e4204c8dd9d7e
headcruser <headcruser@gmail.com>
2017-10-06 16:22:32 -0500

    Creacion de la Clase para administrar las sessiones de los usuarios

---
+++ core/lib/Session/Session.php
@@ -0,0 +1,41 @@
+<?php namespace core\lib\Session;
+
+/*
+* Gestiona las sessiones de los usuarios
+**/
+class Session
+{
+    public function __construct(){
+        session_start();
+    }
+
+    public function addValue(string $key ,$value){
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+    * Obtiene un arrreglo de la superGlobal Session
+    */
+    public function getValue($key)
+    {
+        if($this->issetValue($key))
+           return $_SESSION[$key];           
+        return false;
+    }
+
+    public function removeValue(string $key)
+    {
+        if($this->issetValue($key))
+            unset($_SESSION[$key]);
+    }
+
+    public function issetValue($value):bool
+    {
+        return isset($_SESSION[$value])?true:false;
+    }
+
+    public function destroySession(){
+        session_unset();
+        session_destroy();
+    }
+}
