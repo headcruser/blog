260fb1c873db29264af5cd52a4c75e33323e8b94
headcruser <headcruser@gmail.com>
2017-10-09 16:22:18 -0500

    Se agrego esquema de seguridad en el script Base de datos.

--- core/sql/blog.sql
+++ core/sql/blog.sql
@@ -45,3 +45,48 @@ CREATE TABLE comentarios (
             ON UPDATE CASCADE
             ON DELETE RESTRICT
 );
+
+CREATE TABLE roles(
+    idRoles INT NOT NULL UNIQUE AUTO_INCREMENT,
+    codeRole varchar(5) default 'SR-00',
+    nameRole varchar(45) default 'Sin Asignar',
+    CONSTRAINT PK_ROLES PRIMARY KEY (idRoles)
+);
+
+CREATE TABLE privilegios(
+    idPrivilegio INT NOT NULL UNIQUE AUTO_INCREMENT,
+    codePrivilegio varchar(5) default 'SR-00',
+    namePrivilegio varchar(45) default 'Sin Asignar',
+    CONSTRAINT PK_PRIVILEGIOS PRIMARY KEY (idPrivilegio)
+);
+
+CREATE TABLE usuario_rol(
+    id_role_user INT NOT NULL UNIQUE AUTO_INCREMENT,
+    roles_idRoles INT,
+    usuarios_id int, 
+    CONSTRAINT PK_USUARIO_ROL PRIMARY KEY (id_role_user),
+    CONSTRAINT FK_ROL FOREIGN KEY(roles_idRoles)
+                REFERENCES roles(idRoles)
+                 ON UPDATE CASCADE
+                 ON DELETE RESTRICT,
+    CONSTRAINT FK_USERS FOREIGN KEY(usuarios_id)
+                REFERENCES usuarios(id)
+                 ON UPDATE CASCADE
+                 ON DELETE RESTRICT
+);
+
+
+CREATE TABLE usuario_privilegio(
+    id_users_privilegio INT NOT NULL UNIQUE AUTO_INCREMENT,
+    usuarios_id INT,
+    privilegio_idPrivilegio int, 
+    CONSTRAINT PK_USUARIO_PRIV PRIMARY KEY (id_users_privilegio),
+    CONSTRAINT FK_USERS_PRIV FOREIGN KEY(usuarios_id)
+                REFERENCES usuarios(id)
+                 ON UPDATE CASCADE
+                 ON DELETE RESTRICT,
+    CONSTRAINT FK_PRIV FOREIGN KEY(privilegio_idPrivilegio)
+                REFERENCES privilegios(idPrivilegio)
+                 ON UPDATE CASCADE
+                 ON DELETE RESTRICT
+);
