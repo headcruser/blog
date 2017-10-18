861117fcedd6e8998812f1d0a205def50b9c2818
headcruser <headcruser@gmail.com>
2017-10-18 10:19:07 -0500

    Eliminando Clases inecesarias

--- core/lib/Model/Inflector.php
+++ core/lib/Model/Inflector.php
@@ -11,17 +11,33 @@
   */
 trait Inflector 
 {
-/** 
- * Studly
- * Convierte en una cadena en formato camelCase
- * 
- * @var string snakedString Cadena que sera convertida
- * @return string regresará la misma cadena pero en formato CamelCase
- */
+    /** 
+    * Studly
+    *
+    * Convierte en una cadena en formato camelCase
+    * 
+    * @var string snakedString Cadena que sera convertida
+    * @return string regresará la misma cadena pero en formato CamelCase
+    */
     public function studly(string $snakedString ):string
     {
         $array = explode('_', $snakedString);
         $array = array_map('ucfirst', $array);
         return implode('', $array);
+    }
+    /**
+    * clearInput
+    * 
+    * Limpia una cadena de caracteres de html, espacios
+    * en blanco y de otros caracteres.
+    * 
+    * @return type String Regresa el campo limpio
+    */
+    public static function clearInput(string $campo)
+    {
+        $campo = trim($campo);
+        $campo = stripcslashes($campo);
+        $campo = htmlspecialchars($campo);    
+        return $campo;
     }
 }
